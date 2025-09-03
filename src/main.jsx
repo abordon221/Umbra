091f075fbe9b14adc15f392e50877b8402e79b2c
abordon221 <abordon@gmail.com>
2025-09-02 20:49:58 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+
+const root = document.getElementById('root')
+if (!root) throw new Error('Root element not found')
+
+ReactDOM.createRoot(root).render(<App />)
+
